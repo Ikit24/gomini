52f46e6ebff63a14e1787cffc25c179b77b49b20
Attila <szszattila2@gmail.com>
2026-03-17 19:57:28 +0200

    add: sql table creation for sessions

--- internal/database/sessions.go
+++ internal/database/sessions.go
@@ -1,16 +1,14 @@
 package database
 
 import (
-	"database/sql"
-	"errors"
 	"time"
 
 	"github.com/google/uuid"
 )
 
 type Session struct {
-	ID uuid.UUID        `json:"id"`
-	Title string        `json:"title" db:"chat_title"`
-	CreatedAt time.Time `json:"created_at"`
-	UpdatedAt time.Time `json:"updated_at"`
+	ID        uuid.UUID `json:"id" db:"id"`
+	Title     string    `json:"title" db:"title"`
+	CreatedAt time.Time `json:"created_at" db:"created_at"`
+	UpdatedAt time.Time `json:"updated_at" db:"updated_at"`
 }
---
+++ internal/database/sql/schema.sql
@@ -0,0 +1,6 @@
+CREATE TABLE sessions(
+    id TEXT PRIMARY KEY,
+    title TEXT NOT NULL,
+    created_at DATETIME NOT NULL,
+    updated_at DATETIME NOT NULL
+);
