82f2000f98d36e097a96b2419184c09ee580e586
Attila <szszattila2@gmail.com>
2026-03-21 09:31:17 +0200

    add: open func for db connection

--- internal/database/db.go
+++ internal/database/db.go
@@ -2,8 +2,29 @@ package database
 
 import (
 	"database/sql"
+	"fmt"
 
 	_ "github.com/mattn/go-sqlite3"
 )
 
-func Open() {}
+type DB struct {
+	db *sql.DB
+	path string
+}
+
+func Open(path string) (*DB, error) {
+	dsn := path + "?_foreign_keys=on"
+	conn, err := sql.Open("sqlite3", dsn)
+		if err != nil{
+		fmt.Println("couldn't connect to db:", err)
+		return nil, err
+	}
+	conn.SetMaxOpenConns(1)
+
+	 err = conn.Ping()
+	if err != nil{
+		fmt.Println("couldn't connect to db:", err)
+		return nil, err
+	}
+	return &DB{db : conn, path: path}, nil
+}
