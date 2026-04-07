ca4f8079374d3168f0506ef105ad4c0252df8fc6
Attila <szszattila2@gmail.com>
2026-04-07 20:17:03 +0300

    .

--- internal/database/sessions.go
+++ internal/database/sessions.go
@@ -8,7 +8,12 @@ import (
 
 type Session struct {
 	ID        uuid.UUID `json:"id" db:"id"`
+	UserID    uuid.UUID `json:"user_id" db:"user_id"`
 	Title     string    `json:"title" db:"title"`
 	CreatedAt time.Time `json:"created_at" db:"created_at"`
 	UpdatedAt time.Time `json:"updated_at" db:"updated_at"`
+}
+
+func (d *DB) CreateSession(userID uuid.UUID, title string) (*Session, error) {
+
 }
