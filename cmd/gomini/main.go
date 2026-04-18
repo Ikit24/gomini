172e9ebd194e549c819812318de49789d371f42f
Attila <szszattila2@gmail.com>
2026-04-18 09:14:48 +0300

    rework main.go, started work on http responses

--- cmd/gomini/main.go
+++ cmd/gomini/main.go
@@ -5,11 +5,11 @@ import (
 	"log"
 	"fmt"
 	"context"
+	"net/http"
 
 	"github.com/Ikit24/gomini/internal/database"
 	"github.com/Ikit24/gomini/internal/gemini"
 	"github.com/Ikit24/gomini/internal/handlers"
-	"github.com/google/uuid"
 	"github.com/joho/godotenv"
 )
 
@@ -28,63 +28,13 @@ func main() {
 		log.Fatal("couldn't initialize gemini client", err)
 	}
 
-
-
-
-
 	const dbPath = "gomini.db"
 	db, err := database.Open(dbPath)
 	if err != nil {
-		log.Fatal(err)
+		log.Fatal("couldn't open database", err)
 	}
 	defer db.Close()
 
-	u := &database.User{Name: "Ati"}
-	err = db.CreateUser(u)
-	if err != nil {
-		fmt.Println("couldn't create user:", err)
-		return
-	}
-	fmt.Printf("Created user with ID: %w\n", u.ID)
-
-	foundUser, err := db.GetUserByName("Ati")
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	if foundUser != nil {
-		fmt.Printf("Found user in DB: %s (ID: %d)\n", foundUser.Name, foundUser.ID)
-	} else {
-		fmt.Println("User not found!")
-	}
-
-	sessionID := uuid.New()
-	msg := &database.Message{
-		SessionID: sessionID,
-		Role:      "user",
-		Content:   "Hello! Is this message being saved?",
-	}
-
-	if err := db.SaveMessage(msg); err != nil {
-		log.Fatal("Couldn't save message:", err)
-	}
-
-	fmt.Printf("Successfully save message! (ID: %s)\n", msg.ID)
-
-	history, err := db.GetMessagesBySessionID(sessionID)
-	if err == nil {
-		fmt.Printf("Thread history (%d messages):\n", len(history))
-		for _, m := range history {
-			fmt.Printf("[%s]: %s\n", m.Role, m.Content)
-		}
-	}
-
-	fmt.Printf("Cleaning up session...")
-	err = db.DeleteSession(sessionID)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	finalHistory, _ := db.GetMessagesBySessionID(sessionID)
-	fmt.Printf("History count after deletion: %d\n", len(finalHistory))
+	servr := handlers.NewServer(db, aiClient)
+	log.Fatal(servr.ListenAndServ())
 }
--- internal/handlers/user_handler.go
+++ internal/handlers/user_handler.go
@@ -1,10 +1,44 @@
-package internal
+package handlers
 
 import {
+	"net/http"
+
 	"github.com/Ikit24/gomini/internal/database"
+	"github.com/Ikit24/gomini/internal/gemini"
 }
 
 type Server struct {
 	DB *database.DB
 	AI *gemini.Client
 }
+
+func NewServer(db *database.DB, ai *gemini.Client) *Server {
+	return &Server{
+		DB: db,
+		AI: ai,
+	}
+}
+
+func (http.ResponseWriter, *http.Request) {
+	//
+}
+func (db *database.DB, ai *gemini.Client) *Server() {
+	//to be filled
+}
+
+func (s *Server) ListenAndServe(addr string) error {
+	mux := http.NewServeMux()
+
+	mux.HandleFunc("POST /api/users", s.HandleCreateUser)
+	mux.HandleFunc("POST /api/sessions", s.HandleCreateSession)
+	mux.HandleFunc("POST /api/sessions/{id}/messages", s.HandleCreateSession)
+	mux.HandleFunc("GET /api/sessions/{id}", s.HandleGetSessionByUserID)
+
+	return http.ListenAndServe(addr, mux)
+}
+
+func (s *Server) HandleCreateUser(w http.ResponseWriter, r *http.Request) {}
+
+func (s *Server) HandleCreateSession(w http.ResponseWriter, r *http.Request) {}
+
+func (s *Server) HandleGetSessionByUserID(w http.ResponseWriter, r *http.Request) {}
