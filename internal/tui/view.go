02c54c71565116f6336621fbd0a3027a5a7271c2
Attila <szszattila2@gmail.com>
2026-06-30 05:33:34 +0300

    refactor: text formatter to be more universal

--- internal/tui/view.go
+++ internal/tui/view.go
@@ -10,13 +10,13 @@ var (
 	tooltipPrefix = lipgloss.NewStyle().Bold(true)
 )
 
-func prefixFormatter(tooltip string) string {
-	return tooltipPrefix.Render(tooltip)
+func formatText(style lipgloss.Style, text string) string {
+	return style.Render(text)
 }
 
 func (m Model) View() string {
 	if m.ErrorMessage != "" {
-		return prefixFormatter("Critical error: ") + m.ErrorMessage + prefixFormatter("\nPress [ctrl+c] to quit.")
+		return formatText(tooltipPrefix, "Critical error: ") + m.ErrorMessage + formatText(tooltipPrefix, "\nPress [ctrl+c] to quit.")
 	}
 	switch m.CurrentState {
 	case StateWelcome:
@@ -32,31 +32,31 @@ func (m Model) View() string {
 
 func (m Model) viewBrowse() string {
 	var savedChats string
-	savedChats += prefixFormatter("Saved Chats:") + "\n\n"
+	savedChats += formatText(tooltipPrefix, "Saved Chats:") + "\n\n"
 
 	for i, session := range m.PastSessions {
 		baseText := "SessionID: " + session.ID.String() + " " + "CreatedAt: " + session.CreatedAt.Format("02/01/2006")
 		if i == m.BrowseCursor {
-			savedChats += cursorPrefixColor.Render("> " + baseText) + "\n"
+			savedChats += formatText(cursorPrefixColor, "> " + baseText) + "\n"
 		} else {
 			savedChats += "  " + baseText + "\n"
 		}
 	}
-	savedChats += prefixFormatter("\nPress [esc] to return")
+	savedChats += formatText(tooltipPrefix, "\nPress [esc] to return")
 	return savedChats
 }
 
 func (m Model) viewWelcome() string {
 	var s string
-	s += prefixFormatter("Welcome to Gomini! \n\n")
+	s += formatText(tooltipPrefix, "Welcome to Gomini! \n\n")
 
 	if len(m.PastSessions) > 0 {
 		s += "You have " + fmt.Sprint(len(m.PastSessions)) + " previous conversations.\n"
-		s += prefixFormatter("Press [ctrl+b] to browse your history, or [ctrl+n] to start new chat.")
+		s += formatText(tooltipPrefix, "Press [ctrl+b] to browse your history, or [ctrl+n] to start new chat.")
 	} else {
-		s += prefixFormatter("Press [ctrl+n] to start new chat.")
+		s += formatText(tooltipPrefix, "Press [ctrl+n] to start new chat.")
 	}
-	s += prefixFormatter("\n\nPress [ctrl+c] to quit.")
+	s += formatText(tooltipPrefix, "\n\nPress [ctrl+c] to quit.")
 	//lip gloss later here to center text and add borders
 	return s
 }
