de88fead1a102cd9074d5fe361e8daba7adcc6db
Attila <szszattila2@gmail.com>
2026-07-02 05:58:58 +0300

    initial setup for markdown functionality

--- internal/tui/model.go
+++ internal/tui/model.go
@@ -6,6 +6,7 @@ import (
 	"github.com/Ikit24/gomini/internal/gemini"
 	"github.com/charmbracelet/bubbles/textinput"
 	"github.com/charmbracelet/bubbles/viewport"
+	"github.com/charmbracelet/glamour"
 	tea "github.com/charmbracelet/bubbletea"
 	"github.com/charmbracelet/lipgloss"
 	"github.com/charmbracelet/bubbles/spinner"
@@ -32,6 +33,7 @@ type Model struct {
 	BrowseCursor    int
 	spinner         spinner.Model
 	isLoading       bool
+	renderer        *glamour.TermRenderer
 }
 
 type appState int
@@ -62,7 +64,19 @@ func InitialModel(db *database.DB, client *gemini.Client, userID uuid.UUID, sess
 		CurrentState:    StateWelcome,
 		PastSessions:    sessions,
 		spinner:         s,
+		renderer:        createMarkdownRenderer(TerminalWidth),
 	}
+}
+
+func createMarkdownRenderer(width int) *glamour.TermRenderer {
+	renderer, err := glamour.NewTermRenderer(
+		glamour.WithAutoStyle(),
+		glamour.WithWordWrap(width),
+		)
+	if err != nil {
+		return fmt.Println("Failed to start renderer:", err)
+	}
+	return renderer
 }
 
 func (m Model) Init() tea.Cmd {
--- internal/tui/update.go
+++ internal/tui/update.go
@@ -215,16 +215,16 @@ func (m Model) refreshViewportContent() Model {
 	for _, msg := range m.Messages {
 		if msg.Role == database.UserRole {
 			coloredPrefix := formatText(userPrefixColor, "You: ")
-			s += wordwrap.String(coloredPrefix+msg.Content, safeWidth) + "\n\n"
+			s += wordwrap.String(coloredPrefix + msg.Content, safeWidth) + "\n\n"
 		}
 		if msg.Role == database.ModelRole {
 			coloredPrefix := formatText(gominiPrefixColor, "Gemini: ")
-			s += wordwrap.String(coloredPrefix+msg.Content, safeWidth) + "\n\n"
+			s += wordwrap.String(coloredPrefix + msg.Content, safeWidth) + "\n\n"
 		}
 	}
 	if m.CurrentStream != "" {
 		coloredPrefix := formatText(gominiPrefixColor, "Gemini: ")
-		s += wordwrap.String(coloredPrefix+m.CurrentStream, safeWidth) + "\n"
+		s += wordwrap.String(coloredPrefix + m.CurrentStream, safeWidth) + "\n"
 	}
 	m.Viewport.SetContent(s)
 	m.Viewport.GotoBottom()
