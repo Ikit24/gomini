d87369da17bd75f86478867ed8079b60bd71eeaf
Attila <szszattila2@gmail.com>
2026-08-13 13:08:14 +0300

    feat(database): automate migration setup with embedded sql - Embed SQL migration files into the binary using //go:embed. - Execute migrations automatically on startup using pressly/goose. - Ensure consistent database schema across all environments.

--- internal/database/db.go
+++ internal/database/db.go
@@ -2,8 +2,10 @@ package database
 
 import (
 	"database/sql"
+	"embed"
 
 	_ "github.com/mattn/go-sqlite3"
+	"github.com/pressly/goose/v3"
 )
 
 type DB struct {
@@ -19,15 +21,30 @@ func (d *DB) Ping() error {
 	return d.db.Ping()
 }
 
+//go:embed migrations/*.sql
+var embedMigrations embed.FS
+
 func Open(path string) (*DB, error) {
 	dsn := path + "?_foreign_keys=on"
+
 	conn, err := sql.Open("sqlite3", dsn)
 	if err != nil {
 		return nil, err
 	}
+
 	conn.SetMaxOpenConns(1)
 
 	if err = conn.Ping(); err != nil {
+		return nil, err
+	}
+
+	goose.SetBaseFS(embedMigrations)
+
+	if err := goose.SetDialect("sqlite3"); err != nil {
+		return nil, err
+	}
+	
+	if err := goose.Up(conn, "migrations"); err != nil {
 		return nil, err
 	}
 
